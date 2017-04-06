657b0489ad05a27541081cee8ca85f336db596a6
Mahesh Sawant <Mahesh@Maheshs-MacBook-Pro.local>
2017-04-06 00:09:28 -0700

    local to git

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Container from './App'
+import './index.css';
+
+ReactDOM.render(
+  <Container />,
+  document.getElementById('root')
+);
